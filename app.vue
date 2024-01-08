72f4784fdb762c303f1781700b24626418e58094
Diego Aguilar <diego.aguilar@kiwicredito.com>
2024-01-08 18:15:25 -0500

    First Commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<style>
+* {
+  font-family: 'Poppins', sans-serif;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    fontFamily: {
+      display: ['Poppins'],
+      body: ['Poppins']
+    },
+    extend: {
+      colors: {
+        dark: '#1a202c',
+        light: '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
+
