4022c9befb66fd1d64987ae8a1e0def538bf257c
Diego Aguilar <diego.aguilar@kiwicredito.com>
2024-01-08 19:28:54 -0500

    [feat] first release to new order

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import OrdersView from "../views/OrdersView.vue";
+import OrdersNew from "../views/NewOrderView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: OrdersView,
+    },
+    {
+      path: "/orders/new",
+      name: "OrdersNew",
+      component: OrdersNew,
     },
   ],
 });
