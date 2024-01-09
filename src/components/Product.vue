21a2fffb35b3cdd69eea3c06c96cd5d0588591a0
Diego Aguilar <diego.aguilar@kiwicredito.com>
2024-01-08 21:00:25 -0500

    [feat] add logic

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,12 +1,21 @@
 <template>
-    <div>
-        <img :src="product.imageUrl" alt="" />
+    <div class="max-w-56 bg-white rounded-lg p-4 cursor-pointer">
+        <img :src="props.product.product.image" alt="" @click="addProduct"/>
         <div>
-            <h1>{{ product.name }}</h1>
-            <p>{{ product.price }}</p>
+            <h1>{{ props.product.product.name }}</h1>
+            <p>{{ props.product.product.price }}</p>
         </div>
     </div>
 </template>
 
 <script setup>
+import { useMainStore } from "@/stores/main.store";
+
+const props = defineProps(['product'])
+const mainStore = useMainStore();
+
+const addProduct = () => {
+    mainStore.addProductToOrder(props.product.product);
+}
+
 </script>
